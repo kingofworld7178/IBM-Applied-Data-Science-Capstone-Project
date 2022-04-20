d2cffa84982e3052bab3a567a3c2d76a7558a145
Boddireddy Yashwanth Reddy <75927870+kingofworld7178@users.noreply.github.com>
2022-04-20 21:57:31 +0530

    Add files via upload

---
+++ number_pattern.py
@@ -0,0 +1,11 @@
+x=float(input('enter the no....')) #no.of rows
+I=1
+k=1
+number=x
+while ( I<=x ):
+    while ( k <= number):
+        print (k, end = '  ')
+        k = k + 1
+    number=number + 5
+    I=I+1
+    print("\n")
